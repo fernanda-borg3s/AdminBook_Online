6d364646cbab43dff894baa40276f590277767ea
Fernanda Borges <fernandaborges133@gmail.com>
2025-11-20 11:40:54 -0300

    config deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -46,7 +46,9 @@ const router = createBrowserRouter([
     ]
   },
   
-])
+],
+{ basename: "/AdminBook_Online/" }
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
