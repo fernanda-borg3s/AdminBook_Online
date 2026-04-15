dbc6e030b2c0bea7de5681ec52a43828ec72760d
Fernanda Borges <fernandaborges133@gmail.com>
2026-04-15 19:08:35 -0300

    commenting on unused services

--- frontend/src/services/livrosService.js
+++ frontend/src/services/livrosService.js
@@ -1,32 +1,32 @@
-import api from "./api";
+// import api from "./api";
 
 
-// Objeto ou classe responsável por todas as chamadas relacionadas a Livros
-const livrosService = {
-  // Busca todos os livros
-  getAllLivros: async () => {
-    const response = await api.get("/livros/todosLivros");
-    return response.data;
-  },
-  getLivrosLidos: async () => {
-    const response = await api.get("/livros/livrosLidos");
-    return response.data;
-  },
+// // Objeto ou classe responsável por todas as chamadas relacionadas a Livros
+// const livrosService = {
+//   // Busca todos os livros
+//   getAllLivros: async () => {
+//     const response = await api.get("/livros/todosLivros");
+//     return response.data;
+//   },
+//   getLivrosLidos: async () => {
+//     const response = await api.get("/livros/livrosLidos");
+//     return response.data;
+//   },
 
-  // Buscar um livro específico por ID
-  //   getLivroById: async (id) => {
-  //     const response = await api.get(`/livros/${id}`);
-  //     return response.data;
-  //   },
+//   // Buscar um livro específico por ID
+//   //   getLivroById: async (id) => {
+//   //     const response = await api.get(`/livros/${id}`);
+//   //     return response.data;
+//   //   },
 
-  registerLivro: async (livroData) => {
-    const response = await api.post("/livros/registrarLivro", livroData);
-    return response.data;
-  },
-  //   deleteLivro: async (id) => {
-  //     const response = await api.delete(`/livros/${id}`);
-  //     return response.data;
-  //   }
-};
+//   registerLivro: async (livroData) => {
+//     const response = await api.post("/livros/registrarLivro", livroData);
+//     return response.data;
+//   },
+//   //   deleteLivro: async (id) => {
+//   //     const response = await api.delete(`/livros/${id}`);
+//   //     return response.data;
+//   //   }
+// };
 
-export default livrosService;
+// export default livrosService;
